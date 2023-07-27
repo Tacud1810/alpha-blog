cd263cd4e936f7a5a57ea5c90ca02246618dae2c
Tacud1810 <r.kachyna@gmail.com>
2023-07-27 10:38:01 +0200

    new file:   app/controllers/articles_controller.rb 	new file:   app/controllers/pages_controller.rb 	new file:   app/helpers/pages_helper.rb 	new file:   app/models/article.rb 	new file:   app/views/articles/index.html.erb 	new file:   app/views/articles/show.html.erb 	new file:   app/views/pages/about.html.erb 	new file:   app/views/pages/home.html.erb 	modified:   config/application.rb 	modified:   config/database.yml 	modified:   config/routes.rb 	new file:   db/migrate/20230727050033_create_articles.rb 	new file:   db/schema.rb 	new file:   test/controllers/pages_controller_test.rb

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module AlphaBlog
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Prague'
   end
 end
