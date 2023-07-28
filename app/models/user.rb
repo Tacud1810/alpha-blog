5c6a16ca6f5153a6df5ca74879bf0d4e8d046501
Tacud1810 <r.kachyna@gmail.com>
2023-07-28 18:48:37 +0200

    modified:   app/controllers/articles_controller.rb 	modified:   app/models/article.rb 	modified:   app/models/user.rb 	new file:   db/migrate/20230728151157_add_user_id_to_articles.rb 	modified:   db/schema.rb
    one to many associations between users and articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
+	has_many :articles
 	validates :username, 	presence: true, 
 							uniqueness: { case_sensitive: false }, 
 							length: {minimum: 3, maximum: 25}
-	VALID_EMAIL_REGEX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 	validates :email, 	presence: true, 
 						uniqueness: { case_sensitive: false }, 
-						length: {maximum: 105},
-						format: {with: VALID_EMAIL_REGEX }
+						length: {maximum: 105}
+					
 
 end
