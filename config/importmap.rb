861ed96edb2db8921e33121b7fe3c03e25aa332b
Tacud1810 <r.kachyna@gmail.com>
2023-07-29 13:18:14 +0200

    modified:   app/assets/stylesheets/application.css 	modified:   app/javascript/application.js 	modified:   app/views/articles/_form.html.erb 	modified:   app/views/articles/edit.html.erb 	modified:   app/views/articles/index.html.erb 	modified:   app/views/articles/new.html.erb 	modified:   app/views/articles/show.html.erb 	modified:   app/views/layouts/_footer.html.erb 	modified:   app/views/layouts/_messages.html.erb 	modified:   app/views/layouts/_navigation.html.erb 	modified:   app/views/layouts/application.html.erb 	modified:   app/views/pages/home.html.erb 	new file:   app/views/shared/_errors.html.erb 	modified:   config/environment.rb 	modified:   config/importmap.rb
    new layout of application

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,3 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
