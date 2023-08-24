c1742d5b83649b01e0b00a48bb56d9a64a795aaa
Tacud1810 <r.kachyna@gmail.com>
2023-08-24 08:51:38 +0200

    Changes to be committed: 	modified:   app/controllers/users_controller.rb 	new file:   app/views/articles/_article.html.erb 	modified:   app/views/articles/index.html.erb 	modified:   app/views/users/_form.html.erb 	new file:   app/views/users/edit.html.erb 	new file:   app/views/users/show.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,11 @@ class UsersController < ApplicationController
 	def new
 		@user = User.new
 	end
+		
+	def show
+		@user = User.find(params[:id])
+		@articles = @user.articles
+	end	
 
 	def create
 		@user = User.new(user_params)
@@ -13,8 +18,21 @@ class UsersController < ApplicationController
 		end	
 	end
 
+	def edit
+		@user = User.find(params[:id])
+	end
 
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			flash[:notice] = "Your account was updated."
+			redirect_to articles_path
+		else
+			render 'edit', status: :unprocessable_entity
+		end	
+	end	
 
+	
 	private
 	def user_params
 		params.require(:user).permit(:username, :email, :password)
